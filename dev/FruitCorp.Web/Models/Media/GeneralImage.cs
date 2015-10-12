ddb8662e634dc764c28d880d2e0a57a219640ef7
bashenandi <bashenandi@163.com>
2015-10-12 19:30:47 +0800

    Complete book on the 'FruitCorp' project

---
+++ dev/FruitCorp.Web/Models/Media/GeneralImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace FruitCorp.Web.Models.Media
+{
+    [ContentType(DisplayName = "GeneralImage", GUID = "1e1b0e6e-4cb7-4de4-949a-ce8aa106aba8", Description = "")]
+    /*[MediaDescriptor(ExtensionString = "pdf,doc,docx")]*/
+    public class GeneralImage : ImageData
+    {
+        public virtual String Description { get; set; }
+    }
+}
--- dev/FruitCorp.Web/Models/Pages/StandardPage.cs
+++ dev/FruitCorp.Web/Models/Pages/StandardPage.cs
@@ -1,19 +1,32 @@
 ﻿using EPiServer.Core;
+using System.ComponentModel.DataAnnotations;
 using EPiServer.DataAnnotations;
+using EPiServer.Web;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer;
 
 namespace FruitCorp.Web.Models.Pages
 {
     [ContentType(DisplayName = "StandardPage", GUID = "9238d9f0-71de-4860-ab2c-7b3fb5658219", Description = "")]
-    public class StandardPage : PageData
+    [ImageUrl("~/Static/Icon/xx.png")]
+    public class StandardPage : BasePage
     {
         /// <summary>
         /// 主体介绍
         /// </summary>
+        [UIHint("Textarea")]
+        [UIHint(UIHint.Textarea)]
+        [UIHint(UIHint.Textarea, PresentationLayer.Edit)]
         public virtual string MainIntro { get; set; }
+
+        [UIHint(UIHint.Image)]
+        public virtual Url Image { get; set; }
 
         /// <summary>
         /// 主要内容
         /// </summary>
         public virtual XhtmlString MainBody { get; set; }
+
+        public virtual ContentArea RightColumnContent { get; set; }
     }
 }
